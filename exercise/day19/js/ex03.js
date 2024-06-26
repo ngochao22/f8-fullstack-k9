a68173109f156e45b21717ff74ff3649ccec55a9
Nguyễn Ngọc Hào <6653062@sv.vnua.edu.vn>
2024-06-26 23:25:50 +0700

    sua btvn buoi 9

--- exercise/day19/js/ex03.js
+++ exercise/day19/js/ex03.js
@@ -1,9 +1,23 @@
-var a = ["ngàn", "trăm", "mươi", ""];
-var num = 2143;
-var ns = String(num);
 
-var b = ns
-  .split("")
-  .map((so, i) => so + " " + a[i])
-  .join(" ");
-console.log(b);
+  var m = ["một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín"];
+
+  var a = ["ngàn", "trăm", null, null];
+  var num = 1243;
+  var ns = String(num);
+  
+  var b = ns
+    .split("")
+    .map((so, i) => {
+      var c = a[4 - ns.length + i];
+      var d = c ?  `${c}` : "";
+  
+      var cc = m[so - 1];
+  
+      if (ns.length > 1 && so === "1" && i === ns.length - 1) {
+        cc = "mốt";
+      }
+  
+      return cc + d;
+    })
+    .join(" ");
+  console.log(b);
