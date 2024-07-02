0da3d169613e0e1e96472387b7308c3388c409cc
Nguyễn Ngọc Hào <6653062@sv.vnua.edu.vn>
2024-07-02 23:52:16 +0700

    fix btvn buoi 21

--- exercise/day21/js/ex04.js
+++ exercise/day21/js/ex04.js
@@ -1,5 +1,5 @@
-var numbers = [5, 1, 9, 4, 8, 10, 2, -7];
-var element = 4;
+var numbers = [5, 1, 9, 8, 10, 2, -7];
+var element = -4;
 
 var sortNumber = function (arr) {
     for (var i in arr) {
@@ -20,6 +20,43 @@ var sortNumber = function (arr) {
             arr[minIndex] = temp;
         }
     }
-    return arr;
+    var insertNumber = function (number) {
+        var newNumber = [];
+        newNumber.length = arr.length + 1;
+
+        if (number > arr[arr.length - 1]) {
+            for (var i in arr) {
+                newNumber[i] = arr[i];
+            }
+            newNumber[newNumber.length - 1] = number;
+            return newNumber;
+        }
+
+        if (number < arr[0]) {
+            newNumber[0] = number;
+            for (var i in arr) {
+                newNumber[+i + 1] = arr[i];
+            }
+            return newNumber;
+        }
+
+        var numberIndex = 0;
+        for (var i in arr) {
+            newNumber[i] = arr[i];
+            if (number >= arr[+i - 1] && number <= arr[i]) {
+                newNumber[i] = number;
+                numberIndex = i;
+                break;
+            }
+        }
+
+        for (var i = +numberIndex + 1; i <= newNumber.length - 1; i++) {
+            newNumber[i] = arr[i - 1];
+        }
+
+        document.write(newNumber);
+    };
+    return insertNumber;
 };
-console.log(sortNumber(numbers));
+var insertNumber = sortNumber(numbers);
+insertNumber(element);
